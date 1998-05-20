d2d4a5a2ca81f8f4ea9e2fc04e1398af76032805
efdi <efdi@1738b366-ca78-dd11-ac90-000e8e03bdb1>
1998-05-20 21:19:49 +0000

    Initial revision.
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/trunk/sog@144 1738b366-ca78-dd11-ac90-000e8e03bdb1

---
+++ src/include/quest.h
@@ -0,0 +1,28 @@
+#ifndef __QUEST_H_FILE__
+#define __QUEST_H_FILE__
+
+/* quest rewards */
+#define QUEST_ITEM1 94
+#define QUEST_ITEM2 95
+#define QUEST_ITEM3 96
+
+/* The Quests */
+#define QUEST_EYE		(B)
+#define QUEST_WEAPON		(C)
+#define QUEST_GIRTH		(D)
+#define QUEST_RING		(E)
+#define QUEST_WEAPON2		(F)
+#define QUEST_GIRTH2		(G)
+#define QUEST_RING2		(H)
+#define QUEST_WEAPON3		(I)
+#define QUEST_GIRTH3		(J)
+#define QUEST_RING3		(K)
+
+/* Object vnums for object quest 'tokens': object quest. */
+
+#define QUEST_OBJQUEST1 84
+#define QUEST_OBJQUEST2 85
+#define QUEST_OBJQUEST3 86
+#define QUEST_OBJQUEST4 97
+
+#endif
