03f7a9032d70244d68155b4d4651f0ff79cde426
fjoe <fjoe@1738b366-ca78-dd11-ac90-000e8e03bdb1>
1998-08-06 10:43:26 +0000

    Level affect in do_steal removed at all. :)
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/trunk/sog@553 1738b366-ca78-dd11-ac90-000e8e03bdb1

--- src/mod_dofun/act_obj.c
+++ src/mod_dofun/act_obj.c
@@ -1,5 +1,5 @@
 /*
- * $Id: act_obj.c,v 1.52 1998-08-06 10:03:45 fjoe Exp $
+ * $Id: act_obj.c,v 1.53 1998-08-06 10:43:26 fjoe Exp $
  */
 
 /***************************************************************************
@@ -2202,7 +2202,6 @@ do_steal(CHAR_DATA * ch, const char *argument)
 	WAIT_STATE(ch, skill_table[gsn_steal].beats);
 	percent = number_percent() + (IS_AWAKE(victim) ? 10 : -50);
 	percent += can_see(victim, ch) ? -10 : 0;
-	percent += (victim->level - ch->level)/2;
 
 	if (victim->position == POS_FIGHTING
 	||  (!IS_NPC(ch) && percent > get_skill(ch, gsn_steal))) {
