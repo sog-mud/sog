e308ae5718d8e1d7e49a32fd6621dad42914a453
fjoe <fjoe@1738b366-ca78-dd11-ac90-000e8e03bdb1>
1998-10-17 16:21:47 +0000

    Initial commit.
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/trunk/sog@791 1738b366-ca78-dd11-ac90-000e8e03bdb1

---
+++ src/sog/cmd.c
@@ -0,0 +1,47 @@
+/*-
+ * Copyright (c) 1998 fjoe <fjoe@iclub.nsu.ru>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: cmd.c,v 1.1 1998-10-17 16:21:47 fjoe Exp $
+ */
+
+#include <string.h>
+
+#include "typedef.h"
+#include "cmd.h"
+#include "str.h"
+
+extern CMD_DATA cmd_table[]; /* temporary fix before varr cmd impl */
+
+CMD_DATA *cmd_lookup(const char *name)
+{
+	CMD_DATA *cmd;
+
+	for (cmd = cmd_table; cmd->name; cmd++)
+		if (!str_cmp(cmd->name, name))
+			return cmd;
+
+	return NULL;
+}
+
