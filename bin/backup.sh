5bf85212049ceba3cb0559e3dac6f67e173fddbf
fjoe <fjoe@1738b366-ca78-dd11-ac90-000e8e03bdb1>
2000-03-22 11:25:23 +0000

    SOG_HOME is determined automatically from $0. backup.sh added for pfiles backups.
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/trunk/sog@2278 1738b366-ca78-dd11-ac90-000e8e03bdb1

---
+++ bin/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#
+# determine our home
+# all other paths are relative to $SOG_HOME
+if ! SOG_HOME=$(expr $0 : "\(/.*\)/bin/sog\.sh\$"); then
+	echo "$0: Cannot determine the SOG_HOME" >&2
+	exit 1
+fi
+export SOG_HOME
+
+#
+# determine time stamp
+stamp=`LC_TIME=en_US.ISO_8859-1 date | awk '{ print $3"-"$2"-"$6 }'`
+
+cd $SOG_HOME/player || exit 1
+tar cvfz ../backup/backup-$stamp.tar.gz *
