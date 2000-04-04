b475d0b59d82107736fb3a14a608db2335b9898d
fjoe <fjoe@1738b366-ca78-dd11-ac90-000e8e03bdb1>
2000-04-04 06:26:04 +0000

    backup.sh was unable to determine SOG_HOME.
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/trunk/sog@2378 1738b366-ca78-dd11-ac90-000e8e03bdb1

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,7 +3,7 @@
 #
 # determine our home
 # all other paths are relative to $SOG_HOME
-if ! SOG_HOME=$(expr $0 : "\(/.*\)/bin/sog\.sh\$"); then
+if ! SOG_HOME=$(expr $0 : "\(/.*\)/bin/backup\.sh\$"); then
 	echo "$0: Cannot determine the SOG_HOME" >&2
 	exit 1
 fi
@@ -15,3 +15,4 @@ stamp=`LC_TIME=en_US.ISO_8859-1 date | awk '{ print $3"-"$2"-"$6 }'`
 
 cd $SOG_HOME/player || exit 1
 tar cvfz ../backup/backup-$stamp.tar.gz *
+echo "$0: Backup complete ($SOG_HOME/backup/backup-$stamp.tar.gz)" >&2
