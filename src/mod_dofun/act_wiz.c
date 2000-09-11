e9d868942981c7d38d8a46bd186579ac33084c85
fjoe <fjoe@1738b366-ca78-dd11-ac90-000e8e03bdb1>
2000-09-11 07:09:55 +0000

    MFC: rev. 1.251
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/branches/SOG_1_0/sog@2582 1738b366-ca78-dd11-ac90-000e8e03bdb1

--- src/mod_dofun/act_wiz.c
+++ src/mod_dofun/act_wiz.c
@@ -1,5 +1,5 @@
 /*
- * $Id: act_wiz.c,v 1.186.2.20 2000-08-10 06:57:15 fjoe Exp $
+ * $Id: act_wiz.c,v 1.186.2.21 2000-09-11 07:09:55 fjoe Exp $
  */
 
 /***************************************************************************
@@ -2815,6 +2815,11 @@ void do_string(CHAR_DATA *ch, const char *argument)
 		
 		if (obj->pObjIndex->limit >= 0) {
 			char_puts("You cannot string limited objs.\n", ch);
+			return;
+		}
+
+		if (CAN_WEAR(obj, ITEM_WEAR_CLANMARK)) {
+			char_puts("You cannot string clan marks.\n", ch);
 			return;
 		}
 
