f2f2576f879c3b95973b93b9fb53a4c7b65ecdca
fjoe <fjoe@1738b366-ca78-dd11-ac90-000e8e03bdb1>
2000-10-05 14:51:28 +0000

    fix messages in do_ambush.
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/trunk/sog@2598 1738b366-ca78-dd11-ac90-000e8e03bdb1

--- src/mod_dofun/martial_art.c
+++ src/mod_dofun/martial_art.c
@@ -1,5 +1,5 @@
 /*
- * $Id: martial_art.c,v 1.165 2000-10-05 14:46:39 fjoe Exp $
+ * $Id: martial_art.c,v 1.166 2000-10-05 14:51:28 fjoe Exp $
  */
 
 /***************************************************************************
@@ -1976,7 +1976,12 @@ void do_ambush(CHAR_DATA *ch, const char *argument)
 		return;
 	}
 
-	if (!HAS_INVIS(ch, ID_CAMOUFLAGE) || can_see(victim, ch)) {
+	if (!HAS_INVIS(ch, ID_CAMOUFLAGE)) {
+		act_char("But you are not camouflaged!", ch);
+		return;
+	}
+
+	if (can_see(victim, ch)) {
 		act_char("But they can see you.", ch);
 		return;
 	}
