cfa80837b648c75c1f6dca86142a2ea8f9cc67c7
fjoe <fjoe@1738b366-ca78-dd11-ac90-000e8e03bdb1>
2001-01-18 22:15:49 +0000

    - correctly handle escapes ('\\') - correctly handle msgdb messages without translation (only @eng part present) - skip some functions and '// notrans$'
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/trunk/sog@2719 1738b366-ca78-dd11-ac90-000e8e03bdb1

--- src/checktrans.pl
+++ src/checktrans.pl
@@ -1,14 +1,20 @@
 #!/usr/bin/perl -w
-# $Id: checktrans.pl,v 1.3 2001-01-18 12:15:07 avn Exp $
+#
+# $Id: checktrans.pl,v 1.4 2001-01-18 22:15:49 fjoe Exp $
+#
+# Usage: checktrans.pl [-u] [-d] files...
+# Options:
+#	-u	- do not print unused messages from msgdb
+#	-d	- debug
+#
 
 use strict;
+use Getopt::Std;
+use vars qw/ $opt_u $opt_d /;
 
 # path to msgdb
 my $msgdb_path = "../etc/msgdb";
 #my $msgdb_path = "foo";
-
-# print unused messages
-my $print_unused = 0;
 
 #
 # msgdb: msgdb (read from <STDIN>)
@@ -52,90 +58,112 @@ sub process_file
 	my @string;
 	my $start_line;
 	my $start_pos;
-	my $end_line;
-	my $end_pos;
 
 	#
 	# parse flags
-	my $search_comments_end = 0;
-	my $parse_string = 0;
-	my $seen_something = 1;
+	my $search_eoc = 0;		# we are searching end of
+					# multiline comments
+	my $search_eos = 0;		# we are searching end of string
+	my $seen_eos = 0;		# we just finished parsing a string
 
 	READ_LINE: while (<INPUT>) {
 		$linenum++;
-		@line = split //;
 
+		#
 		# skip macro processor directives
 		next if (/^#/);
 
-		my $prev_char;
+		# skip some specific strings
+		next if (/(KEY|aff_write_list|skill_beats|str_printf|yyerror)\("/);
+		next if (/(IS_TOKEN|check_improve|db_set_arg|do_help|fwrite_string|fprintf|fwrite_word|get_skill|mlstr_dump|mlstr_fwrite|log|skill_mana|str_cmp|str_cscmp|strcmp)\([a-zA-Z_][a-zA-Z0-9_+()]*, "/);
+		next if (/dofun\("help", [a-zA-Z_][a-zA-Z0-9_]*, "/);
+		next if (m|// notrans$|);
+
+		my $seen_escape = 0;	# seen escape character ('\\')
 		my $i = 0;
+		@line = split //;
 		PARSE_LINE: while ($i <= $#line) {
 			#
 			# check for comments
-			#
-			if ($search_comments_end) {
+			if ($search_eoc) {
+				#
+				# check for end of multiline comments
 				if ($line[$i] eq "*" && $line[$i+1] eq "/") {
-#					print "$linenum: end of multiline comments\n";
+					print STDERR "end of multiline comments $linenum:$i\n" if $opt_d;
 					$i++;
 
-					$search_comments_end = 0;
+					$search_eoc = 0;
 					next PARSE_LINE;
 				}
 				next PARSE_LINE;
 			} elsif ($line[$i] eq "/") {
+				#
+				# check for '//' comments
 				last PARSE_LINE if ($line[$i+1] eq "/");
+
+				#
+				# check for start of multiline comments
 				if ($line[$i+1] eq "*") {
 					$i++;
 
-					$search_comments_end = 1;
+					$search_eoc = 1;
 					next PARSE_LINE;
 				}
 			}
 
-			if ($parse_string) {
-				if ($line[$i] eq '"') {
-					$parse_string = 0;
+			if ($search_eos) {
+				#
+				# check for end of string
+				if ($line[$i] eq '"' && !$seen_escape) {
+					#
+					# eos found. we delay finilazing
+					# the string because there can be
+					# string continuation just after some
+					# space characters
+					$search_eos = 0;
+					$seen_eos = 1;
+					print STDERR "supposed end of string: $linenum:$i\n" if $opt_d;
 					next PARSE_LINE;
-				} elsif ($line[$i] eq "\\" && $line[$i+1]) {
-					push @string, $line[$i++];
 				}
 
+				#
+				# append character to current string
 				push @string, $line[$i];
 				next PARSE_LINE;
-			} elsif ($line[$i] eq '"') {
-				if ($seen_something) {
-#					print "Start of string: $linenum:$i\n";
+			} elsif ($line[$i] eq '"' && !$seen_escape) {
+				if (!$seen_eos) {
+					#
+					# is not continuation
+					print STDERR "start of string: $linenum:$i\n" if $opt_d;
 					$start_line = $linenum;
 					$start_pos = $i;
-					$seen_something = 0;
 				}
 
-				$parse_string = 1;
+				#
+				# just a continuation
+				$search_eos = 1;
 				next PARSE_LINE;
 			}
 
-			if (index("\n\r\t ", $line[$i]) < 0
-			&&  !$seen_something) {
-#				print "End of string: $linenum:$i\n";
-				$end_line = $linenum;
-				$end_pos = $i;
-
+			if ($seen_eos && index("\n\r\t ", $line[$i]) < 0) {
+				#
+				# we just found an eos and it is not
+				# space character
+				print STDERR "end of string: $linenum:$i\n" if $opt_d;
 				push @{$file_strings{join '', @string}}, $start_line;
 				$#string = -1;
-				$seen_something = 1;
+				$seen_eos = 0;
 			}
 		} continue {
-			$prev_char = $line[$i];
-			$i++;
+			$seen_escape = ($line[$i++] eq '\\') && !$seen_escape;
 		}
 	} continue {
-#		print "$linenum: ", join '', @line;
+		print STDERR "$linenum: ", join '', @line if $opt_d;
 	}
 
-#	print "All the strings:\n";
+	print STDERR "All the strings:\n" if $opt_d;
 	foreach my $i (keys %file_strings) {
-#		print "string: $i, line(s): @{$file_strings{$i}}\n";
+		print STDERR "string: $i, line(s): @{$file_strings{$i}}\n" if $opt_d;
 		my $nonl = $i;
 		$nonl =~ s/\\n$//;
 		if (exists $msgdb{$i}) {
@@ -166,56 +194,70 @@ sub dump_messages
 	print "\n";
 }
 
+sub add_message
+{
+	my $msg = shift;
+
+	$msg =~ s/~~/~/g;
+	$msg =~ s/^\.//;
+	$msgdb{$msg} = {};
+
+	return 0;
+}
+
+#---------------------------------------------------------------------
+# main program
+#
+
+getopts('ud');
+
 #
 # read msgdb from STDIN
 my $msg = "";
-my $search_rus = 0;
+my $search_rus = 0;		# we are searching for @rus
 open(IN, "<$msgdb_path");
 while (<IN>) {
 	chomp;
 	last if (/^\$~$/);
-	if ($search_rus) {
+	if (s/^\@eng //) {
+		if ($search_rus) {
+			# we are still searching for '@rus '
+			# save old message
+			$search_rus = add_message($msg);
+			print STDERR "Found \@rus, saved message without translation: '$msg'\n" if $opt_d;
+		}
+
+		# save current message
+		if (s/(.*)\@rus .*/$1/) {
+			# found and stripped @rus
+			add_message($_);
+			print STDERR "Saved one-line message: '$_'\n" if $opt_d;
+			next;
+		}
+
+		print STDERR "Found new message start: '$_'\n" if $opt_d;
+		$msg = $_ . "\\n";
+		$search_rus = 1;
+		next;
+	} elsif ($search_rus) {
 		if (s/(.*)\@rus .*/$1/) {
 			# we are searching for '@rus ' and have found it
-			$msg = $msg . $_;
-			$msg =~ s/~~/~/g;
-			$msg =~ s/^\.//;
-			$msgdb{$msg} = {};
-#			print "Found \@rus, saved message: '$msg'\n";
-
-			$search_rus = 0;
+			$search_rus = add_message($msg . $_);
+			print STDERR "Found \@rus, saved message: '$msg'\n" if $opt_d;
 			next;
 		}
 		$msg = $msg . $_ . "\\n";
 		next;
 	}
-	if (s/^\@eng //) {
-		# save current message
-		if (s/(.*)\@rus .*/$1/) {
-			# found and stripped @rus
-			s/~~/~/g;
-			s/^\.//;
-			$msgdb{$_} = {};
-#			print "Saved one-line message: '$_'\n";
-			next;
-		}
-
-#		print "Found new message start: '$_'\n";
-		$msg = $_ . "\\n";
-		$search_rus = 1;
-		next;
-	}
 }
 close(IN);
 
-#my @msgdb_eng = keys %msgdb;
-#$" = "\n";
-#print "All english msgdb messages:\n";
-#print "@msgdb_eng\n";
-
-if ($ARGV[0] eq '-u') {
-	$print_unused = 1;
-	shift @ARGV;
+if ($opt_d) {
+	my @msgdb_eng = keys %msgdb;
+	print STDERR "All english msgdb messages:\n";
+	$" = "\n";
+	print STDERR "@msgdb_eng\n";
+	$" = " ";
 }
 
 #
@@ -224,7 +266,7 @@ foreach my $i (@ARGV) {
 	process_file($i);
 }
 
-if ($print_unused) {
+if ($opt_u) {
 	print "===> Unused messages:\n";
 	foreach my $i (keys %msgdb) {
 		next if (%{$msgdb{$i}});
