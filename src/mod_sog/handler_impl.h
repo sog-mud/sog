4e588f5e99379e6805444cbc833cad17b2b05e99
fjoe <fjoe@1738b366-ca78-dd11-ac90-000e8e03bdb1>
2001-08-05 16:45:28 +0000

    - log.c does not #include module headers anymore
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/trunk/sog@2996 1738b366-ca78-dd11-ac90-000e8e03bdb1

---
+++ src/mod_sog/handler_impl.h
@@ -0,0 +1,34 @@
+/*-
+ * Copyright (c) 2001 SoG Development Team
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: handler_impl.h,v 1.1 2001-08-05 16:45:28 fjoe Exp $
+ */
+
+#ifndef _HANDLER_IMPL_H_
+#define _HANDLER_IMPL_H_
+
+void act_char_logger(CHAR_DATA *ch, const char *alias, const char *msg);
+
+#endif /* _HANDLER_IMPL_H_ */
