1f039846548551bcfadc5d438322dc2c5399aba1
fjoe <fjoe@1738b366-ca78-dd11-ac90-000e8e03bdb1>
2001-09-23 18:43:31 +0000

    ugh, new random code is not in SoG 1
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/branches/SOG_1_0/sog@3259 1738b366-ca78-dd11-ac90-000e8e03bdb1

--- bin/rndcontrol.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# $Id: rndcontrol.sh,v 1.2.2.1 2001-09-23 18:18:57 fjoe Exp $
-
-rndcontrol=/usr/sbin/rndcontrol
-[ -x $rndcontrol ] && $rndcontrol -s 10 -s 14 > /dev/null && echo -n ' rnd'
