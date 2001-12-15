9fa6ba9360f584cb8966cfeddd5a81f065951e4c
(no author) <(no author)@1738b366-ca78-dd11-ac90-000e8e03bdb1>
2001-12-15 13:47:54 +0000

    This commit was manufactured by cvs2svn to create branch 'SOG_1_0'.
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/branches/SOG_1_0/sog@3514 1738b366-ca78-dd11-ac90-000e8e03bdb1

---
+++ src/include/mccp.h
@@ -0,0 +1,54 @@
+/*-
+ * Copyright (c) 2001 SoG Development Team
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: mccp.h,v 1.1 2001-12-15 13:47:47 matrim Exp $
+ */
+
+/*
+ * mccp.c - support functions for mccp (the Mud Client Compression Protocol)
+ *
+ * see http://homepages.ihug.co.nz/~icecube/compress/ and README.Rom24-mccp
+ *
+ * Copyright (c) 1999, Oliver Jowett <icecube@ihug.co.nz>.
+ *
+ * This code may be freely distributed and used if this copyright notice is
+ * retained intact.
+ */
+
+#ifndef _MCCP_H_
+#define _MCCP_H_
+
+#include <zlib.h>
+
+#define TELOPT_COMPRESS		85
+#define TELOPT_COMPRESS2	86
+#define COMPRESS_BUF_SIZE	16384
+
+void *	zlib_alloc(void *opaque, unsigned int items, unsigned int size);
+void	zlib_free(void *opaque, void *address);
+bool	processCompressed(DESCRIPTOR_DATA *desc);
+bool	writeCompressed(DESCRIPTOR_DATA *desc, const char *txt, int length);
+
+#endif
