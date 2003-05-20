67483b80c4438644a3ddf60868e9c20f5b4049eb
fjoe <fjoe@1738b366-ca78-dd11-ac90-000e8e03bdb1>
2003-05-20 21:40:54 +0000

    exclude '*.core' and 'core', pass --exclude params with '' (not "")
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/trunk/sog@4286 1738b366-ca78-dd11-ac90-000e8e03bdb1

--- bin/cvs-rsync.sh
+++ bin/cvs-rsync.sh
@@ -5,5 +5,6 @@ local_cvsroot=/usr/local/cvs/SoG-CVS/
 
 #
 # compression is turned on in ~/.ssh/config (see ssh_config)
-rsync -e ssh -av --delete --exclude ".#cvs.*" --delete-excluded \
+rsync -e ssh -av --delete \
+	--exclude '.#cvs.*' --exclude '*.core' --exclude 'core' --delete-excluded \
 	cvs-sog:/usr/local/cvsroot/ ${local_cvsroot}
