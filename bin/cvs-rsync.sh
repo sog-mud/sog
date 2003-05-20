0d3c9e6cfff2d60dd20dd1a64e80dad4a2c36a39
fjoe <fjoe@1738b366-ca78-dd11-ac90-000e8e03bdb1>
2003-05-20 20:52:54 +0000

    add a script for rsyncing CVS
    git-svn-id: file:///home/fjoe/old-repos/svn/husky/trunk/sog@4285 1738b366-ca78-dd11-ac90-000e8e03bdb1

---
+++ bin/cvs-rsync.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# NOTE: trailing slashes are significant
+local_cvsroot=/usr/local/cvs/SoG-CVS/
+
+#
+# compression is turned on in ~/.ssh/config (see ssh_config)
+rsync -e ssh -av --delete --exclude ".#cvs.*" --delete-excluded \
+	cvs-sog:/usr/local/cvsroot/ ${local_cvsroot}
